3c1c76d81681369ed59c489db0b8e56838b9b046
Proto <nat10peter@gmail.com>
2026-02-07 19:35:52 -0700

    Added Shooting and projectiles, Next up is to add map collision and projectile collision.

--- graphics_test.cpp
+++ graphics_test.cpp
@@ -5,6 +5,13 @@
 
 #define PLAYER_SPD 200.0f
 
+#define MAX_PROJECTILES 100
+#define PROJECTILES_SPD 400.0f
+#define PROJECTILES_RADIUS 5.0f
+
+
+
+
 typedef struct Player {
     Vector2 position;
     float speed;
@@ -16,8 +23,22 @@ typedef struct EnvItem {
     Color color;
 } EnvItem;
 
+typedef struct Projectile{
+    Vector2 position;
+    Vector2 velocity;
+    float radius;
+    bool active;
+}Projectile;
+
+//Player and Camera
 void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta);
 void UpdateCameraCenter(Camera2D *camera, Player *player, EnvItem *envItems, int envItemsLength, float delta, int width, int height);
+
+//Weapons
+void Attack(Player *player, Vector2 mousePosition, Projectile *projectiles, int maxProjectiles); //add mouse direction, weapon type etc.
+void UpdateProjectiles(Projectile *projectiles, int maxProjectiles, float delta);
+void DrawProjectiles(Projectile *projectiles, int maxProjectiles);
+
 
 
 int main(void){
@@ -30,6 +51,12 @@ int main(void){
     player.position = (Vector2){400, 280};
     player.speed = 0;
     
+    Projectile projectiles[MAX_PROJECTILES] = {0};
+    for (int i = 0; i < MAX_PROJECTILES; i++) {
+        projectiles[i].active = false;
+    }
+
+
     EnvItem envItems[] = {
         {{ 0, 0, 1000, 400 }, 0, LIGHTGRAY },
         {{ 0, 400, 1000, 200 }, 1, GRAY },
@@ -56,8 +83,13 @@ int main(void){
 
     while(!WindowShouldClose()){
         float deltaTime = GetFrameTime();
+        Vector2 mouse = GetMousePosition();
+        Vector2 mousePos = GetScreenToWorld2D(mouse, camera);
+
+
 
         UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
+        UpdateProjectiles(projectiles, MAX_PROJECTILES, deltaTime);
         UpdateCameraCenter(&camera, &player, envItems, envItemsLength, deltaTime, screenWidth, screenHeight);
         
         BeginDrawing();
@@ -72,9 +104,15 @@ int main(void){
 
                 DrawCircleV(player.position, 5.0f, GOLD);
 
+                DrawProjectiles(projectiles, MAX_PROJECTILES);
+
             EndMode2D();
 
             DrawText("Send Help", 0, 20, 15, BLACK);
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                Attack(&player, mousePos, projectiles, MAX_PROJECTILES);
+            }
 
         EndDrawing();
     }
@@ -89,6 +127,13 @@ void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float d
     if (IsKeyDown(KEY_RIGHT)) player->position.x += PLAYER_SPD*delta;
     if (IsKeyDown(KEY_DOWN)) player->position.y += PLAYER_SPD*delta;
     if (IsKeyDown(KEY_UP)) player->position.y -= PLAYER_SPD*delta;
+
+    if (IsKeyDown(KEY_A)) player->position.x -= PLAYER_SPD*delta;
+    if (IsKeyDown(KEY_D)) player->position.x += PLAYER_SPD*delta;
+    if (IsKeyDown(KEY_S)) player->position.y += PLAYER_SPD*delta;
+    if (IsKeyDown(KEY_W)) player->position.y -= PLAYER_SPD*delta;
+
+
 
     bool hitObstacle = false;
     for (int i = 0; i < envItemsLength; i++)
@@ -115,3 +160,42 @@ void UpdateCameraCenter(Camera2D *camera, Player *player, EnvItem *envItems, int
     camera->target = player->position;
 }
 
+void Attack(Player *player, Vector2 mousePosition, Projectile *projectiles, int maxProjectiles){
+    for (int i = 0; i < maxProjectiles; i++){
+        if(!projectiles[i].active){
+            projectiles[i].active = true;
+            projectiles[i].position = player ->position;
+            projectiles[i].radius = PROJECTILES_RADIUS;
+            
+            Vector2 direction = Vector2Subtract(mousePosition, player->position);
+
+            // Normalizes direction to keep speed normal
+            direction = Vector2Normalize(direction);
+
+            projectiles[i].velocity = Vector2Scale(direction, PROJECTILES_SPD);
+
+            break;
+        }
+    }
+}
+
+void UpdateProjectiles(Projectile *projectiles, int maxProjectiles, float delta){
+    for (int i = 0; i < maxProjectiles; i++){
+        if (projectiles[i].active){
+            projectiles[i].position.x += projectiles[i].velocity.x * delta;
+            projectiles[i].position.y += projectiles[i].velocity.y * delta;
+
+            if (projectiles[i].position.x < -100 || projectiles[i].position.x > 1100 || projectiles[i].position.y < -100 || projectiles[i].position.y > 700) {
+                    projectiles[i].active = false;
+            }
+        }
+    }
+}
+
+void DrawProjectiles(Projectile *projectiles, int maxProjectiles){
+    for (int i = 0; i<maxProjectiles; i++){
+        if(projectiles[i].active){
+            DrawCircleV(projectiles[i].position, projectiles[i].radius, RED);
+        }
+    }
+}
